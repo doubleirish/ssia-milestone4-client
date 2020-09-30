4e9f54ca72c99c72b2e3fbe8240753e2fa76c24f
Conor Redmond <doubleirish@users.noreply.github.com>
2020-09-29 21:03:38 -0700

    skeleton app initialized

---
+++ src/main/java/com/manning/ssia/milestone4_client/Milestone4ClientApplication.java
@@ -0,0 +1,13 @@
+package com.manning.ssia.milestone4_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Milestone4ClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Milestone4ClientApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/manning/ssia/milestone4_client/Milestone4ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.ssia.milestone4_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Milestone4ClientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
